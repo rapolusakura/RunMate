a3f6d2b1d41c7e2928e08aee81c9a8d7b628a6c6
Sakura Rapolu <rapolusakura@gmail.com>
2018-07-31 11:54:04 -0700

    set up initial view controller

--- RunMate/RunMate/ViewController.swift
+++ RunMate/RunMate/ViewController.swift
@@ -7,19 +7,21 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
+    let apiKey = "AIzaSyDP_vdpdBSqJobAnUOJTz-hlYKlHKQwDYw"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let parameters = ["key":apiKey,"location":"34.0254248,-118.2873805","radius":"3218.69","type":"park"]
+        Alamofire.request("https://maps.googleapis.com/maps/api/place/nearbysearch/json?", parameters: parameters).responseJSON(options:.mutableContainers) {response in
+            let response = try! JSON(data: response.data!)
+            print(response)
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
