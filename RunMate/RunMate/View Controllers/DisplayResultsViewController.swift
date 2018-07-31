42ee95e9264665901a1c870a8d26236d46609d3d
Sakura Rapolu <rapolusakura@gmail.com>
2018-07-31 12:07:33 -0700

    added displayResultsViewController

---
+++ RunMate/RunMate/View Controllers/DisplayResultsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  DisplayResultsViewController.swift
+//  RunMate
+//
+//  Created by Sakura Rapolu on 7/31/18.
+//  Copyright © 2018 MakeSchool. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DisplayResultsViewController: UIViewController {
+    
+}
