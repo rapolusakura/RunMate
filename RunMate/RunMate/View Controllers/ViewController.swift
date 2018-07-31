bc1cad901d4507f64e6f64258a1445f59391da9f
Sakura Rapolu <rapolusakura@gmail.com>
2018-07-31 12:04:01 -0700

    updated info.plist

--- RunMate/RunMate/View Controllers/ViewController.swift
+++ RunMate/RunMate/View Controllers/ViewController.swift
@@ -11,17 +11,10 @@ import Alamofire
 import SwiftyJSON
 
 class ViewController: UIViewController {
-    let apiKey = "AIzaSyDP_vdpdBSqJobAnUOJTz-hlYKlHKQwDYw"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        let parameters = ["key":apiKey,"location":"34.0254248,-118.2873805","radius":"3218.69","type":"park"]
-        Alamofire.request("https://maps.googleapis.com/maps/api/place/nearbysearch/json?", parameters: parameters).responseJSON(options:.mutableContainers) {response in
-            let response = try! JSON(data: response.data!)
-            print(response)
-        }
     }
 }
 
