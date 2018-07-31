5a31e860c7e6acbec1bd5e9cd6b52160a0512e8f
Sakura Rapolu <rapolusakura@gmail.com>
2018-07-31 12:36:26 -0700

    fixed table

--- RunMate/RunMate/View Controllers/DisplayResultsViewController.swift
+++ RunMate/RunMate/View Controllers/DisplayResultsViewController.swift
@@ -9,6 +9,17 @@
 import Foundation
 import UIKit
 
-class DisplayResultsViewController: UIViewController {
+class DisplayResultsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        return cell
+    }
+    
     
 }
