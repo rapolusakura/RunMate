7e3c1009b27152c23ec1de2b4cdf3a39ddc410a9
Sakura Rapolu <rapolusakura@gmail.com>
2018-08-03 11:30:49 -0700

    can make the yelp request

--- RunMate/RunMate/View Controllers/ViewController.swift
+++ RunMate/RunMate/View Controllers/ViewController.swift
@@ -14,7 +14,26 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let apiToContact = "https://api.yelp.com/v3/businesses/search?term=tourist_attractions&location=chicago,il"
+        guard let url = URL(string: apiToContact) else {return assertionFailure("URL Failed")}
+        
+        var request = URLRequest(url: url)
+        request.setValue("Bearer 704azzpEPQYj74YGBz1mwE0yp96vB5VjV9ytQluciKI0EC46ZA6kprJMwgml2ffxqPngLUmlkh2jBK6t3c4eOcMqPSjvyurPthNZo1jyS-HgBfUl2bYsTVUQPJFkW3Yx", forHTTPHeaderField: "Authorization")
+        
+        Alamofire.request(request).validate().responseJSON() { response in
+            switch response.result {
+            case .success:
+                if let value = response.result.value {
+                    
+                    let json = JSON(value)
+                    print(json)
+                    
+                }
+            case .failure(let error):
+                print(error)
+            }
         // Do any additional setup after loading the view, typically from a nib.
+        }
     }
     
     @IBAction func viewResultsButton(_ sender: Any) {
