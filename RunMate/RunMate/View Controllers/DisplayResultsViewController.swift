02b18937a5744b59e2a030cec93326db36be04d9
Sakura Rapolu <rapolusakura@gmail.com>
2018-08-06 11:33:34 -0700

    make cells rounded but kind of ugly

--- RunMate/RunMate/View Controllers/DisplayResultsViewController.swift
+++ RunMate/RunMate/View Controllers/DisplayResultsViewController.swift
@@ -15,20 +15,43 @@ class DisplayResultsViewController: UIViewController, UITableViewDelegate, UITab
     @IBOutlet weak var tableView: UITableView!
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
         return routes.count
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 25
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView()
+        headerView.backgroundColor = UIColor.clear
+        return headerView
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! DisplayResultTableViewCell
-        let route = routes[indexPath.row]
+        let route = routes[indexPath.section]
         cell.placeNameLabel.text = route.place.name
         cell.distanceLabel.text = String(format: "%.2f", Conversion.metersToMiles(meters: route.distance))
         + " mi"
+        cell.layer.borderColor = UIColor.black.cgColor
+        cell.layer.borderWidth = 1
+        cell.layer.cornerRadius = 8
+        cell.clipsToBounds = true
+        
+        cell.layer.shadowOffset = CGSize(width: 1, height: 0)
+        cell.layer.shadowColor = UIColor.black.cgColor
+        cell.layer.shadowRadius = 5
+        cell.layer.shadowOpacity = 0.8
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        var route = routes[indexPath.row]
+        let route = routes[indexPath.section]
         ElevationServices.findElevationDifference(startLat: route.startLat, startLng: route.startLng, endLat: (route.place.lat), endLng: (route.place.lng)) { (elevation) in
             route.elevation = elevation
             self.performSegue(withIdentifier: "showPlaceDetails", sender: route)
