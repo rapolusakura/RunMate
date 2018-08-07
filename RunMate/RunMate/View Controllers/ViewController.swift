276599d57fa326ed15741fc0348d9226ae398b16
Sakura Rapolu <rapolusakura@gmail.com>
2018-08-06 20:23:47 -0700

    fixed bug where you cant view results if you dont enter a distance

--- RunMate/RunMate/View Controllers/ViewController.swift
+++ RunMate/RunMate/View Controllers/ViewController.swift
@@ -61,35 +61,40 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var viewResultsOutlet: UIButton!
     
     @IBAction func viewResultsButton(_ sender: Any) {
-        let miles = Double(distanceTextField.text!)!
-        let distance = Conversion.milestoMeters(miles: miles) //distance in meters
-        let coordinate = locationManager.location?.coordinate
-        locationManager.stopUpdatingLocation()
-        let travelModeIndex = travelModeSegmentedControl.selectedSegmentIndex
-        let travelMode: String
-        switch travelModeIndex {
-        case 1:
-            travelMode = "bicycling"
-        default:
-            travelMode = "walking"
-        }
-        let tripSettingIndex = tripSettingSegmentedControl.selectedSegmentIndex
-        switch tripSettingIndex {
-        case 1:
-            PlacesService.findRoundTripNearbyPlaces(lat: 37.7808727, lng: -122.4183261, originalRadius: distance, travelMode: travelMode) { (routes) in
-                self.performSegue(withIdentifier: "viewResults", sender: routes)
+        if let miles = Double(distanceTextField.text!) {
+            let distance = Conversion.milestoMeters(miles: miles) //distance in meters
+            let coordinate = locationManager.location?.coordinate
+            locationManager.stopUpdatingLocation()
+            let travelModeIndex = travelModeSegmentedControl.selectedSegmentIndex
+            let travelMode: String
+            switch travelModeIndex {
+            case 1:
+                travelMode = "bicycling"
+            default:
+                travelMode = "walking"
             }
-//            PlacesService.findRoundTripNearbyPlaces(lat: (coordinate?.latitude)!, lng: (coordinate?.longitude)!, originalRadius: distance, travelMode: travelMode) { (routes) in
-//                self.performSegue(withIdentifier: "viewResults", sender: routes)
-//            }
-        default:
-            PlacesService.findOneWayNearbyPlaces(lat: 37.7808727, lng: -122.4183261, radius: distance, travelMode: travelMode) { (routes) in
-                self.performSegue(withIdentifier: "viewResults", sender: routes)
+            let tripSettingIndex = tripSettingSegmentedControl.selectedSegmentIndex
+            switch tripSettingIndex {
+            case 1:
+                PlacesService.findRoundTripNearbyPlaces(lat: 37.7808727, lng: -122.4183261, originalRadius: distance, travelMode: travelMode) { (routes) in
+                    self.performSegue(withIdentifier: "viewResults", sender: routes)
+                }
+                //            PlacesService.findRoundTripNearbyPlaces(lat: (coordinate?.latitude)!, lng: (coordinate?.longitude)!, originalRadius: distance, travelMode: travelMode) { (routes) in
+                //                self.performSegue(withIdentifier: "viewResults", sender: routes)
+            //            }
+            default:
+                PlacesService.findOneWayNearbyPlaces(lat: 37.7808727, lng: -122.4183261, radius: distance, travelMode: travelMode) { (routes) in
+                    self.performSegue(withIdentifier: "viewResults", sender: routes)
+                }
+                //            PlacesService.findOneWayNearbyPlaces(lat: (coordinate?.latitude)!, lng: (coordinate?.longitude)!, radius: distance, travelMode: travelMode) { (routes) in
+                //                self.performSegue(withIdentifier: "viewResults", sender: routes)
+                //            }
             }
-//            PlacesService.findOneWayNearbyPlaces(lat: (coordinate?.latitude)!, lng: (coordinate?.longitude)!, radius: distance, travelMode: travelMode) { (routes) in
-//                self.performSegue(withIdentifier: "viewResults", sender: routes)
-//            }
         }
+        else {
+            print("button wass pressed but there was no distance value")
+        }
+        
     }
     
     @IBAction func viewPreviousRoutesButtonPressed(_ sender: Any) {
@@ -121,7 +126,6 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             print("i dont recognize this")
         }
     }
-    
 }
 
 extension UISegmentedControl {
